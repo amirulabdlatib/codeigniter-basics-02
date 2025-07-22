40498722f12196a3dac00f4ef98c8968cdacb772
Amirul <amirulal73@gmail.com>
2025-07-22 19:57:45 +0800

    Add create article form

--- app/Views/Articles/create.php
+++ app/Views/Articles/create.php
@@ -4,4 +4,15 @@
 <?= $this->section('content'); ?>
 
 <h1>New article</h1>
+<?= form_open("articles/store"); ?>
+
+<label for="title">Title</label>
+<input type="text" id="title" name="title">
+
+<label for="content">Content</label>
+<textarea type="text" id="content" name="content"></textarea>
+
+<button type="submit">Create</button>
+
+</form>
 <?= $this->endSection(); ?>
