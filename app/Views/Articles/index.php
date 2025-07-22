b926a92fbc337939846037778c1f5a26a20829a0
Amirul <amirulal73@gmail.com>
2025-07-22 17:13:58 +0800

    Display dynamic data

--- app/Views/Articles/index.php
+++ app/Views/Articles/index.php
@@ -5,4 +5,10 @@
 <?= $this->section("content"); ?>
 <h1>Articles index page</h1>
 
+<?php foreach ($articles as $article): ?>
+    <article>
+        <h2><?= $article['title']; ?></h2>
+        <p><?= $article['content']; ?></p>
+    </article>
+<?php endforeach; ?>
 <?= $this->endSection(); ?>
