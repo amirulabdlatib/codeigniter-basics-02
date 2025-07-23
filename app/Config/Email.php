c44029c731781b30beb0a7a9eea8ac4fe3b57a4f
Amirul <amirulal73@gmail.com>
2025-07-23 19:46:54 +0800

    Setup shield

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'amirul@example.com';
+    public string $fromName   = 'Amirul';
     public string $recipients = '';
 
     /**
