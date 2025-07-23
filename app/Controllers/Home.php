361f91c7ebae2a3cfb3831259d1ae911af76b578
Amirul <amirulal73@gmail.com>
2025-07-23 20:21:23 +0800

    Disable test email

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        $this->sendTestEmail();
+        // $this->sendTestEmail();
         return view("Home/index.php");
     }
 
