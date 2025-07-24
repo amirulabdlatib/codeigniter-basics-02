7dc75c2aab84bfc27527fe86e37c0ae2c37308d0
Amirul <amirulal73@gmail.com>
2025-07-24 12:42:00 +0800

    Output the logged in user

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -5,6 +5,7 @@
 <?= $this->section("content"); ?>
 <h1>Welcome</h1>
 <?php if (auth()->loggedIn()): ?>
+    <p>Hello <?= esc(auth()->user()->first_name); ?></p>
     <a href="<?= url_to("logout"); ?>">Logout</a>
 <?php else: ?>
     <a href="<?= url_to("login"); ?>">Login</a>
