7a71fb7ccac1151482110a29a9c7cb9f5b7b1b67
Amirul <amirulal73@gmail.com>
2025-07-24 19:53:13 +0800

    Add simple navbar

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -4,11 +4,6 @@
 
 <?= $this->section("content"); ?>
 <h1>Welcome</h1>
-<?php if (auth()->loggedIn()): ?>
-    <p>Hello <?= esc(auth()->user()->first_name); ?></p>
-    <a href="<?= url_to("logout"); ?>">Logout</a>
-<?php else: ?>
-    <a href="<?= url_to("login"); ?>">Login</a>
-<?php endif; ?>
+
 
 <?= $this->endSection(); ?>
--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -9,6 +9,23 @@
 </head>
 
 <body>
+    <nav>
+        <a href="<?= url_to("/"); ?>">Home</a>
+        <?php if (auth()->loggedIn()): ?>
+
+            <a href="<?= url_to("Articles::index"); ?>">Articles</a>
+
+            <a href="<?= url_to("\Admin\Controllers\Users::index"); ?>">Users</a>
+
+            <a href="<?= url_to("logout"); ?>">Logout</a>
+            Hello <?= esc(auth()->user()->first_name); ?>
+
+        <?php else: ?>
+            <a href="<?= url_to("login"); ?>">Login</a>
+        <?php endif; ?>
+    </nav>
+
+
     <?php if (session()->has("message")): ?>
         <p><?= session("message"); ?></p>
     <?php endif; ?>
