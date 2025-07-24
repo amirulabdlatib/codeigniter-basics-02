4d169afc99c29c36a1b99ed449072b3283b1bf6f
Amirul <amirulal73@gmail.com>
2025-07-24 15:04:10 +0800

    Add admin index page

--- Admin/Controllers/Users.php
+++ Admin/Controllers/Users.php
@@ -3,12 +3,24 @@
 namespace Admin\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Users extends BaseController
 {
+    protected UserModel $model;
+
+    public function __construct()
+    {
+        $this->model = new UserModel;
+    }
+
     public function index()
     {
-        echo "Hello from the user admin index";
+        $users = $this->model->findAll();
+
+        return view("Admin\Views\Users\index", [
+            "users" => $users,
+        ]);
     }
 }
---
+++ Admin/Views/Users/index.php
@@ -0,0 +1,26 @@
+<?= $this->extend("layouts/default"); ?>
+
+<?= $this->section("title"); ?>Users<?= $this->endSection(); ?>
+
+<?= $this->section("content"); ?>
+<h1>Users</h1>
+<table>
+    <thead>
+        <tr>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Active</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($users as $user): ?>
+            <tr>
+                <td><?= esc($user->email); ?></td>
+                <td><?= esc(!empty($user->first_name) ? $user->first_name : 'No name provided'); ?></td>
+                <td><?= esc($user->active ? 'Active' : "Inactive"); ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+<?= $this->endSection(); ?>
