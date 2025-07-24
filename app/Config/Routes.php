bd8cb877106e80f7bd9daaa5c61970a4fd538d72
Amirul <amirulal73@gmail.com>
2025-07-24 12:47:16 +0800

    Add reset password page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,5 +14,7 @@ $routes->get('/articles/(:num)/edit', 'Articles::edit/$1');
 $routes->put('/articles/(:num)', 'Articles::update/$1');
 $routes->delete('/articles/(:num)', 'Articles::delete/$1');
 $routes->get('/articles/delete/(:num)', 'Articles::delete/$1');
+$routes->get("set-password", "Password::set");
+
 
 service('auth')->routes($routes);
