1922301059914e1ff0fbc8fe6ee696ef32566924
Amirul <amirulal73@gmail.com>
2025-07-25 00:30:05 +0800

    Add group permission

--- app/Views/Articles/show.php
+++ app/Views/Articles/show.php
@@ -7,10 +7,10 @@
 <h1><?= $article->title; ?></h1>
 <p><?= $article->content; ?></p>
 
-<?php if ($article->isOwner() || auth()->user()->hasPermission("articles.edit")): ?>
+<?php if ($article->isOwner() || auth()->user()->can("articles.edit")): ?>
     <a href="<?= url_to('Articles::edit', $article->id); ?>">Edit</a>
 <?php endif; ?>
-<?php if ($article->isOwner() || auth()->user()->hasPermission("articles.delete")): ?>
+<?php if ($article->isOwner() || auth()->user()->can("articles.delete")): ?>
     <a href="<?= url_to('Articles::delete', $article->id); ?>">Delete</a>
 <?php endif; ?>
 
